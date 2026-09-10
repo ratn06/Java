9b38ee82894a0767664ee9fcae07f62a954966a6
Ratn Sanjay <ratnsanjay006@gmail.com>
2026-09-10 23:19:12 +0530

    Added HiiHii.java

---
+++ HiiHii.java
@@ -0,0 +1,42 @@
+public class HiiHii {
+
+    
+    public static void main(String[] args) {
+        int marks = (int) 32.7;
+
+        if (marks >= 90)
+        {
+            System.out.println("The Student Pass With Grade 'A+' ");
+        }
+        else if (marks >= 80)
+        {
+            System.out.println("The Student Pass With Grade 'A+' ");
+        }
+        else if (marks >=70 )
+        {
+            System.out.println("The Student Pass With Grade 'B+' ");
+        }
+        else if (marks >=60)
+        {
+            System.out.println("The Student Pass With Grade 'B' " );
+        
+        }
+        else if (marks >=50)
+        {
+            System.out.println("The Student Pass With Grade 'C+' ");
+        }
+        else if (marks >=40)
+        {
+            System.out.println("The Student Pass With Grade 'C' " );
+        }
+        else if (marks >= 33)
+        {
+            System.out.println("The Student Pass With Grade 'D' ");
+        }
+        else
+        {
+            System.out.println("The Student Fail in the Exams  " );
+        }
+    }
+}
+
